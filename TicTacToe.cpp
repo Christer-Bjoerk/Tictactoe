50bf657e398836d17fd5fa44eda0af666af280e8
Christer Bjoerk <productivity.christerbjoerk@gmail.com>
2023-02-07 14:46:01 +0100

    Display board function completed
    Draw the board using chars

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <stdlib.h>
 
+using namespace std;
+
+char board[3][3] = { {'1','2','3'},{'4','5','6'},{'7','8','9'} };
+int choice;
+int row, column;
+char turn = 'X';
+bool draw = false;
+
+void display_board()
+
+{
+	// Drawing the board using characters
+	cout << " PLAYER 1 [X]\t PLAYER 2 [O]\n";
+	cout << "\t     |     |    \n";
+	cout << "\t  " << board[0][0] << "  |  " <<board[0][1] << "  |  " << board[0][2] << " \n";
+	cout << "\t_____|_____|_____\n";
+	cout << "\t     |     |    \n";
+	cout << "\t  " << board[1][0] << "  |  " << board[1][1] << "  |  " << board[1][2] << " \n";
+	cout << "\t_____|_____|_____\n";
+	cout << "\t     |     |    \n";
+	cout << "\t  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << " \n";
+}
+
 int main() 
 {
+	display_board();
 	return 0;
 }
