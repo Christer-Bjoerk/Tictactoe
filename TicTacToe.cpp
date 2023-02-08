81379eab35489f644dae83f67f2beae62b00db92
Christer Bjoerk <productivity.christerbjoerk@gmail.com>
2023-02-08 08:33:18 +0100

    Switch player's turn

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -10,7 +10,6 @@ char turn = 'X';
 bool draw = false;
 
 void display_board()
-
 {
 	// Drawing the board using characters
 	cout << " PLAYER 1 [X]\t PLAYER 2 [O]\n";
@@ -23,6 +22,55 @@ void display_board()
 	cout << "\t     |     |    \n";
 	cout << "\t  " << board[2][0] << "  |  " << board[2][1] << "  |  " << board[2][2] << " \n";
 }
+
+void player_turn()
+{
+	if (turn == 'X')
+	{
+		cout << "\n Player 1 [X] turn : ";
+	}
+	else if (turn == 'O')
+	{
+		cout << "\n Player 2 [O] turn : ";
+	}
+
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1: row = 0; column = 0; break;
+	case 2: row = 0; column = 1; break;
+	case 3: row = 0; column = 2; break;
+	case 4: row = 1; column = 0; break;
+	case 5: row = 1; column = 1; break;
+	case 6: row = 1; column = 2; break;
+	case 7: row = 2; column = 0; break;
+	case 8: row = 2; column = 1; break;
+	case 9: row = 2; column = 2; break;
+	default:
+		cout << "Invalid move";
+		break;
+	}
+
+	if (turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O')
+	{
+		board[row][column] = 'X';
+		turn = 'O';
+	}
+	else if (turn == 'O' && board[row][column] != 'O' && board[row][column] != 'X')
+	{
+		board[row][column] = 'O';
+		turn = 'X';
+	}
+	else
+	{
+		cout << "Box already filled!\n Please choose another!! \n";
+		player_turn();
+	}
+
+	display_board();
+
+};
 
 int main() 
 {
