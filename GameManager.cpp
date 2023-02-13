61731e6812cf8db88cb2cbfc999ac749d5ec616c
Christer Bjoerk <productivity.christerbjoerk@gmail.com>
2023-02-13 17:48:41 +0100

    Diagonal win

--- GameManager.cpp
+++ GameManager.cpp
@@ -2,6 +2,7 @@
 
 bool GameManager::GameOver()
 {
+	// Check for a straight win
 	for (int i = 0; i < 3; i++)
 	{
 		if (target->boardArea[i][0] == target->boardArea[i][1] && target->boardArea[i][0] == target->boardArea[i][2] ||
@@ -11,6 +12,12 @@ bool GameManager::GameOver()
 		}
 	}
 
+	// Check for a win diagonal
+	if (target->boardArea[0][0] == target->boardArea[1][1] && target->boardArea[0][0] == target->boardArea[2][2] ||
+		target->boardArea[0][2] == target->boardArea[1][1] && target->boardArea[0][2] == target->boardArea[2][0])
+	{
+		return true;
+	}
 
 
     return false;
